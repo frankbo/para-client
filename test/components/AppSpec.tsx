a975bd0f39821ed5a592a0ca48d6624bfba3ec4c
Frank <frank.boeddeker@gmx.net>
2016-10-03 19:30:13 +0200

    Add first test for App component
    - currently the reference for the index.d.ts is attached to the head of
      the test. In future test files it would be necessary to add this line
      to every new test file.
    
    Sill looking for a way to globally set this option

--- test/components/AppSpec.tsx
+++ test/components/AppSpec.tsx
@@ -1,17 +1,17 @@
+/// <reference path='../../typings/index.d.ts'/>
+
 import * as React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
+import { EmailField } from '../../app/components/EmailField';
 
 import { App } from '../../app/components/App';
 
 describe('App Component', function () {
     it('should render a form element', function () {
         const app = shallow(<App />);
-        // const form = app.find('form');
-        const foo: Object = { bar: 'baz'};
+        const form = app.find(EmailField);
 
-        expect(foo).to.deep.equal({ bar: 'baz' });
-
-        // expect(form).to.have.className('form');
+        expect(form).to.have.prop('label', 'Your Email');
     })
 });
